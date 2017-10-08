89060dc5e97e0e05892bb49a81a8c33215cdf950
Shoma SATO <noir.neo.04@gmail.com>
2017-10-08 22:56:11 +0900

    Implement ios speech framework bridge plugin

---
+++ Assets/UniSpeech/Sample/Scripts/UniSpeechSample.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace UniSpeech.Sample
+{
+    public class UniSpeechSample : MonoBehaviour, ISpeechRecognizer
+    {
+        void Start()
+        {
+            SpeechRecognizer.CallbackGameObjectName = gameObject.name;
+            SpeechRecognizer.RequestRecognizerAuthorization();
+        }
+
+        public void OnRecognized(string transcription)
+        {
+            Debug.Log("OnRecognized: " + transcription);
+        }
+
+        public void OnError(string description)
+        {
+            Debug.Log("OnError: " + description);
+        }
+
+        public void OnAuthorized()
+        {
+            Debug.Log("OnAuthorized");
+            SpeechRecognizer.StartRecord();
+        }
+
+        public void OnUnauthorized()
+        {
+            Debug.Log("OnUnauthorized");
+        }
+
+        public void OnAvailable()
+        {
+            Debug.Log("OnAvailable");
+        }
+
+        public void OnUnavailable()
+        {
+            Debug.Log("OnUnavailable");
+        }
+    }
+}
+
